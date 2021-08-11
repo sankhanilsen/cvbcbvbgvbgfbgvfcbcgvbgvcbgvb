37bfbb0d117dff3f9622c998058da295c4855739
sankhanilsen <76916705+sankhanilsen@users.noreply.github.com>
2021-08-11 21:53:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function page_one() {
+    window.location = "page_one.html";
+}
+function page_3() {
+    window.location = "pagecar.html";
+}
+function page_2() {
+    window.location = "pagecar - Copy.html";
+}
+function page_one() {
+    window.location = "page_one.html";
+}
+
